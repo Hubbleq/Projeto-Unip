dda1953cfdb57b40137773bec4a05bda2a3d99c5
Hubble <63984897+Hubbleq@users.noreply.github.com>
2024-05-16 18:45:50 -0300

    Add files via upload

--- site/script.js
+++ site/script.js
@@ -61,3 +61,20 @@ $(document).ready(function () {
         }
     });
 });
+
+
+        function openTab(tabName) {
+            var i, tabContent, tabLinks;
+            tabContent = document.getElementsByClassName("tab-content");
+            for (i = 0; i < tabContent.length; i++) {
+                tabContent[i].style.display = "none";
+            }
+            tabLinks = document.getElementsByClassName("tab-btn");
+            for (i = 0; i < tabLinks.length; i++) {
+                tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+            }
+            document.getElementById(tabName).style.display = "block";
+            event.currentTarget.className += " active";
+        }
+
+
